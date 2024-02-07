c571c36cf8bb89c7b21e45d7ebe3fe8acba50cc6
sindrekvande <sindrehk2000@hotmail.com>
2024-02-07 10:30:19 +0100

    Interrupt changes

--- interrupt/src/main.c
+++ interrupt/src/main.c
@@ -27,9 +27,8 @@
 #define MAX_REC_COUNT		1
 #define NDEF_MSG_BUF_SIZE	128
 
-#define NFC_FIELD_LED		DK_LED1
+#define SLEEP_LED			17
 #define SYSTEM_ON_LED		DK_LED2
-#define LED					DK_LED3
 
 #define SLEEP_PIN			13
 #define WAKEUP_PIN			14
@@ -37,8 +36,12 @@
 
 static void input_pin_handle(unsigned int pin, nrfx_gpiote_trigger_t action, void * p)
 {
-	printk("Handler");
-	nrf_gpio_pin_toggle(LED); // Toggle LED on interrupt
+	printk("Handler \n");
+	nrf_gpio_pin_toggle(SLEEP_LED); // Toggle LED on interrupt
+	
+	printk("Powering off.\n");
+	dk_set_led_off(SYSTEM_ON_LED);
+	sys_poweroff();
 }
 
 
@@ -49,14 +52,14 @@ static void gpio_init()
 	err_code = nrfx_gpiote_init(0); // Initialize the GPIOTE
 	printk("Error (0x%X)\n", err_code); // Check for errors
 
-	nrf_gpio_cfg_output(LED); // Initialize the LED
-	nrf_gpio_pin_set(LED); // Turn off the LED
+	nrf_gpio_cfg_output(SLEEP_LED); // Initialize the LED
+	nrf_gpio_pin_set(SLEEP_LED); // Turn off the LED
 
 	nrfx_gpiote_input_config_t in_config;
 	in_config.pull = NRF_GPIO_PIN_PULLUP;
 	
 	nrfx_gpiote_trigger_config_t trig_config;
-	trig_config.trigger = NRF_GPIOTE_POLARITY_TOGGLE;
+	trig_config.trigger = NRF_GPIOTE_POLARITY_HITOLO;
 
 	nrfx_gpiote_handler_config_t hand_config;
 	hand_config.handler = &input_pin_handle;
@@ -67,8 +70,25 @@ static void gpio_init()
 	nrfx_gpiote_trigger_enable(SLEEP_PIN, true); // Enable interrupt events
 }
 
+static void system_off()
+{
+	printk("Powering off.\n");
+
+	dk_set_led_off(SYSTEM_ON_LED);
+
+	sys_poweroff();
+}
+
 int main(void)
 {
+	/* Configure LED-pins */
+	if (dk_leds_init() < 0) {
+		printk("Cannot init LEDs!\n");
+		return 0;
+	}
+
+	dk_set_led_on(SYSTEM_ON_LED);
+
 	gpio_init();
 
 	while (true)
