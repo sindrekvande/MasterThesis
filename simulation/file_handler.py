b3f86381bd12c8306b85ad190f84e2709b7878e1
krissch97 <krissch97@gmail.com>
2024-02-12 14:39:49 +0100

    sim

--- simulation/file_handler.py
+++ simulation/file_handler.py
@@ -1,12 +1,12 @@
 import csv
 import pandas as pd
 #import import_main #LIGHT OFF
-import parameters as pm
-import messages as msg
-from datetime import datetime
+#import parameters as pm
+#import messages as msg
+#from datetime import datetime
 
-inputFilePath = "/home/pi/Desktop/SpecializationProject/datasets/"
-outputFilePath = "/home/pi/Desktop/SpecializationProject/measurements/"
+inputFilePath = "datasets/"
+#outputFilePath = "/home/pi/Desktop/SpecializationProject/measurements/"
 
 #inputFilePath = "C:/Users/kriss/Desktop/NTNU/SpecializationProject/datasets/"
 #outputFilePath = "C:/Users/kriss/Desktop/NTNU/SpecializationProject/measurements/"
@@ -14,41 +14,41 @@ outputFilePath = "/home/pi/Desktop/SpecializationProject/measurements/"
 #headers = ['time','irradiance', 'led_percent', 'SoCpacks', 'adc1.1', 'adc1.2', 'adc1.3', 'adc1.4', 'adc1.5', 'adc1.6', 'adc1.7', 'adc1.8', 'adc2.1', 'adc2.2', 'adc2.3', 'adc2.4', 'adc2.5', 'adc2.6', 'adc2.7', 'adc2.8']
 
 class file:
-    def __init__(self):
-        self.inputFile = inputFilePath + pm.season + ".tsv"
+    def __init__(self, season):
+        self.inputFile = inputFilePath + season + ".tsv"
 
         self.read_from_file()
         self.filter_NaN_values()
 
-        currentDatetime = datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
-        strCurrentDatetime = str(currentDatetime)
+#        currentDatetime = datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+#        strCurrentDatetime = str(currentDatetime)
 
-        self.outputFile = outputFilePath + pm.season + "_measurements_" + strCurrentDatetime + ".tsv"
+#        self.outputFile = outputFilePath + pm.season + "_measurements_" + strCurrentDatetime + ".tsv"
         
-        self.create_output_file()
+#        self.create_output_file()
         
-    def create_output_file(self):
-        #file = open(self.outputFile, "w")
-        #file.close()
-        #self.append_to_file(headers)
-        df = pd.DataFrame({ key:pd.Series(value) for key, value in msg.messages.items() })
-        df.to_csv(self.outputFile, index=False, sep="\t")
+#    def create_output_file(self):
+#        #file = open(self.outputFile, "w")
+#        #file.close()
+#        #self.append_to_file(headers)
+#        df = pd.DataFrame({ key:pd.Series(value) for key, value in msg.messages.items() })
+#        df.to_csv(self.outputFile, index=False, sep="\t")
 
-    def append_to_file(self):#, df: pd.DataFrame):
-        #with open(self.outputFile, "a") as file:
-        #    csv.writer(file, delimiter='\t').writerow(list) 
-        #    file.close()
-        currentDatetime = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-        msg.messages[msg.timeStamp] = str(currentDatetime)
-        df = pd.DataFrame({ key:pd.Series(value) for key, value in msg.messages.items() })
-        df.to_csv(self.outputFile, mode='a', index=False, header=False, sep="\t")
+#    def append_to_file(self):#, df: pd.DataFrame):
+#        #with open(self.outputFile, "a") as file:
+#        #    csv.writer(file, delimiter='\t').writerow(list) 
+#        #    file.close()
+#        currentDatetime = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+#        msg.messages[msg.timeStamp] = str(currentDatetime)
+#        df = pd.DataFrame({ key:pd.Series(value) for key, value in msg.messages.items() })
+#        df.to_csv(self.outputFile, mode='a', index=False, header=False, sep="\t")
     
     def read_from_file(self):
-        self.brightnessDF= pd.read_csv(self.inputFile, sep='\t', usecols = [pm.column],  dtype = float, nrows = (pm.numberOfDays*60*24)) # header=0, index_col=False,
+        self.brightnessDF= pd.read_csv(self.inputFile, sep='\t', usecols = ['Gg_pyr'],  dtype = float, nrows = (pm.numberOfDays*60*24)) # header=0, index_col=False,
         return self.brightnessDF
 
     def single_value(self, index):
-        return self.brightnessDF.loc[index, pm.column]
+        return self.brightnessDF.loc[index, 'Gg_pyr']
 
     def filter_NaN_values(self):
         NaNCounter = 0
@@ -64,7 +64,7 @@ class file:
                         if pd.notna(nextValue):
                             break
             if NaNCounter > 0:
-                self.brightnessDF.loc[key, pm.column] = (previousValue + nextValue)/2
+                self.brightnessDF.loc[key, 'Gg_pyr'] = (previousValue + nextValue)/2
                 NaNCounter -= 1
                     
         return self.brightnessDF
--- simulation/sim.py
+++ simulation/sim.py
@@ -1,3 +1,4 @@
+import file_handler as fh
 # take in solar trace
 # feed solar trace to simulated energy storage
 # do work (measure, communicate, sleep) in state machine
@@ -11,30 +12,70 @@ class energyStorage:
     # E = (Q ⋅ V) / 2
     def __init__(self, capacitance, maxVolt):
         self.maxEnergy = 1/2 * capacitance * maxVolt
+        self.voltage = 0
+        self.energy = 0
 
     def addEnergy(self, irr):
-        pass
+        self.energy += irr * 60
 
     def useEnergy(self, energy):
-        pass
+        if self.energy < energy:
+            self.energy = 0
+        else:
+            self.energy -= energy
+        return self.energy
 
-class stateMachine:
-    self.measureEnergyUse = 
-    self.communicateEnergyUse = 
-    self.sleepEnergyUse = 
+class states:
+    self.measureCurrent       = 8 * 10 ** -3
+    self.communicateCurrent   = 9 * 10 ** -3
+    self.sleepCurrent         = 1.9 * 10 ** -6
     def __init__(self, measTime, comTime, sleepTime):
         self.measTime   = measTime
         self.comTime    = comTime
         self.sleepTime  = sleepTime
 
     def measure(self):
-        energyUsage = self.measureEnergyUse * self.measTime
+        energyUsage = self.measureCurrent * self.measTime
         return energyUsage
         
     def communicate(self):
-        energyUsage = self.measureEnergyUse * self.measTime
+        energyUsage = self.communicationCurrent * self.comTime
         return energyUsage
 
     def sleep(self):
-        energyUsage = self.measureEnergyUse * self.measTime
+        energyUsage = self.sleepCurrent * self.sleepTime
         return energyUsage
+
+class checkpoint:
+    def __init__(self):
+        pass
+
+    def save():
+        pass
+
+    def recover():
+        pass
+
+def main():
+    trace = fh.file("summer").brightnessDF
+    capacitor = energyStorage()
+    states = states()
+    JITsvs = checkpoint()
+    JITadc = checkpoint()
+    interval = checkpoint()
+
+    for key, irrValue in trace.intertuples():
+        capacitor.addEnergy(irrValue)
+        counter = 60
+        nextstate = "measure"
+            while counter > 0:
+                match nextstate:
+                    case "measure":
+                        counter -= states.measure()
+                        nextstate = "communicate"
+                    case "communicate":
+                        counter -= states.communicate()
+                        nextstate = "sleep"
+                    case "sleep":
+                        counter -= states.sleep()
+                        nextstate = "measure"
