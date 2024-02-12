f7605c146691d7261a61a7ba0b20e29ee5a15d31
krissch97 <krissch97@gmail.com>
2024-02-12 12:56:42 +0100

    Update sim.py

--- simulation/sim.py
+++ simulation/sim.py
@@ -1,3 +1,40 @@
 # take in solar trace
 # feed solar trace to simulated energy storage
 # do work (measure, communicate, sleep) in state machine
+
+def get_solar_trace():
+    # use file_handler
+    pass
+
+class energyStorage:
+    # C = (ε0 * A) / d
+    # E = (Q ⋅ V) / 2
+    def __init__(self, capacitance, maxVolt):
+        self.maxEnergy = 1/2 * capacitance * maxVolt
+
+    def addEnergy(self, irr):
+        pass
+
+    def useEnergy(self, energy):
+        pass
+
+class stateMachine:
+    self.measureEnergyUse = 
+    self.communicateEnergyUse = 
+    self.sleepEnergyUse = 
+    def __init__(self, measTime, comTime, sleepTime):
+        self.measTime   = measTime
+        self.comTime    = comTime
+        self.sleepTime  = sleepTime
+
+    def measure(self):
+        energyUsage = self.measureEnergyUse * self.measTime
+        return energyUsage
+        
+    def communicate(self):
+        energyUsage = self.measureEnergyUse * self.measTime
+        return energyUsage
+
+    def sleep(self):
+        energyUsage = self.measureEnergyUse * self.measTime
+        return energyUsage
