49831e7182323e73f41bc64c7e75a6f7f98d08a7
sindrekvande <sindrehk2000@hotmail.com>
2024-02-26 22:35:31 +0100

    Checkpoint update
    Able to write to flash and read from. Also able to retain some RAM from flash. Not able to do whole RAM, maybe due to unwritable adresses.

--- nRF52840/checkpoint/src/checkpoint.c
+++ nRF52840/checkpoint/src/checkpoint.c
@@ -1,45 +1,46 @@
 #include "checkpoint.h"
 
+const struct device *flash_dev = PARTITION_DEVICE;
+uint32_t checkpoint_data[CHECKPOINT_WORDS] = {0};
 
 int checkpoint_create() {
-    const struct device *flash_dev = PARTITION_DEVICE;
-    uint32_t checkpoint_data[CHECKPOINT_WORDS] = {0};
+    printk("#### CREATING CHECKPOINT ###\n");
     get_program_state(checkpoint_data);
 
+    uint32_t offset = PARTITION_OFFSET;
+    if (flash_erase(flash_dev, offset, FLASH_PAGE_SIZE) != 0) {
+        printk("Flash erase failed at offset 0x%X\n", offset);
+        return -1;
+    }
+
     for (uint32_t i = 0; i < CHECKPOINT_WORDS; i++) {
-		uint32_t offset = PARTITION_OFFSET + (i * sizeof(uint32_t));
-		printk("Writing %X at 0x%X\n", checkpoint_data[i], offset);
-		if (flash_write(flash_dev, offset, &checkpoint_data[i], sizeof(checkpoint_data[i])) != 0) {
-			printk("Flash write failed!\n");
+		if (flash_write(flash_dev, offset + (i * sizeof(uint32_t)), &checkpoint_data[i], sizeof(checkpoint_data[i])) != 0) {
+			printk("Flash write failed at offset 0x%08X\n", offset + (i * sizeof(uint32_t)));
 			return -1;
 		}
-        printk("Wrote %X at address 0x%X\n", checkpoint_data[i], offset);
+        printk("Wrote 0x%08X at address 0x%08X\n", checkpoint_data[i], offset + (i * sizeof(uint32_t)));
     }
-    return 0;
+    return save_ram_to_flash();
 }
 
 int checkpoint_recover() {
-    const struct device *flash_dev = PARTITION_DEVICE;
-    uint32_t checkpoint_data[CHECKPOINT_WORDS];
+    printk("#### RECOVERING CHECKPOINT ####\n");
+    uint32_t offset = PARTITION_OFFSET;
 
     for (uint32_t i = 0; i < CHECKPOINT_WORDS; i++) {
-		uint32_t offset = PARTITION_OFFSET + (i * sizeof(uint32_t));
-        printk("Reading 0x%X\n", offset);
-		if (flash_read(flash_dev, offset, &checkpoint_data[i], sizeof(checkpoint_data[i])) != 0) {
-			printk("Flash read failed!\n");
+		if (flash_read(flash_dev, offset + (i * sizeof(uint32_t)), &checkpoint_data[i], sizeof(checkpoint_data[i])) != 0) {
+			printk("Flash read failed at offset 0x%08X\n", offset + (i * sizeof(uint32_t)));
 			return -1;
 		}
-		printk("Read %X, from address %X\n", checkpoint_data[i], offset);
-        set_program_state(checkpoint_data);
+		printk("Read 0x%08X, from address 0x%08X\n", checkpoint_data[i], offset + (i * sizeof(uint32_t)));
     }
-    return 0;
+    set_program_state(checkpoint_data);
+    return retrieve_ram_from_flash();
 }
 
 void get_program_state(uint32_t * buf) { // Number of stored values needs to match CHECKPOINT_WORDS
-    // Register data
-    //for (int i = 0; i <= 12; ++i) {
-    //    __asm__ volatile("MOV %0, R%d\n": "=r" (buf[i]) : "r" (i) : );
-    //}
+    printk("#### GET PROGRAM STATE ####\n");
+
     __asm__ volatile("MOV %0, R0" : "=r" (buf[0]) : : );
     __asm__ volatile("MOV %0, R1" : "=r" (buf[1]) : : );
     __asm__ volatile("MOV %0, R2" : "=r" (buf[2]) : : );
@@ -59,18 +60,12 @@ void get_program_state(uint32_t * buf) { // Number of stored values needs to mat
     __asm__ volatile("MOV %0, LR\n": "=r" (buf[15]) : : );
     __asm__ volatile("MOV %0, PC\n": "=r" (buf[16]) : : );
 
-    // RAM data
-
-
-    // I/O data
-
+    //print_buffer(buf, CHECKPOINT_WORDS);
 }
 
 void set_program_state(uint32_t * buf) {
-    // Register data
-    //for (int i = 0; i <= 12; ++i) {
-    //    __asm__ volatile("MOV R%d, %0\n":  "r" (i) : "=r" (buf[i]));
-    //}
+    printk("#### SET PROGRAM STATE ####\n");
+
     __asm__ volatile("MOV R0, %0" : : "r" (buf[0]) : );
     __asm__ volatile("MOV R1, %0" : : "r" (buf[1]) : );
     __asm__ volatile("MOV R2, %0" : : "r" (buf[2]) : );
@@ -90,50 +85,52 @@ void set_program_state(uint32_t * buf) {
     __asm__ volatile("MOV LR, %0\n": : "r" (buf[15]) : );
     __asm__ volatile("MOV PC, %0\n": : "r" (buf[16]) : );
 
-    // RAM data
-    
-    // I/O data
+    //print_buffer(buf, CHECKPOINT_WORDS);
 }
 
 int save_ram_to_flash() {
-    const struct device *flash_dev = PARTITION_DEVICE;
-    uint32_t offset = RAM_STORAGE_OFFSET; // Offset in flash where RAM data should be stored
+    printk("#### SAVE RAM TO FLASH ####\n");
+    uint32_t offset = PARTITION_OFFSET + CHECKPOINT_WORDS * sizeof(uint32_t); 
 
     for (uint32_t *ram_ptr = (uint32_t *)RAM_START; ram_ptr < (uint32_t *)RAM_END; ram_ptr++) {
-        if (flash_write(flash_dev, offset, ram_ptr, sizeof(uint32_t)) != 0) {
-            printk("Flash write failed at offset 0x%X\n", offset);
+        if (offset % FLASH_PAGE_SIZE == 0) {
+            if (flash_erase(flash_dev, offset, FLASH_PAGE_SIZE) != 0) {
+                printk("Flash erase failed at offset 0x%08X\n", offset);
+                return -1;
+            }
+        }
+        if (flash_write(flash_dev, offset, ram_ptr, sizeof(uint32_t)) != 0) { 
+            printk("Flash write failed at offset 0x%08X\n", offset);
             return -1;
         }
+        printk("Wrote %d from RAM %p at FLASH 0x%08X\n", *ram_ptr, ram_ptr, offset);
         offset += sizeof(uint32_t);
     }
     return 0;
 }
 
 int retrieve_ram_from_flash() {
-    const struct device *flash_dev = PARTITION_DEVICE;
-    uint32_t offset = RAM_STORAGE_OFFSET; // Offset in flash where RAM data is stored
+    printk("#### RETRIVE RAM FROM FLASH ####\n");
+    uint32_t offset = PARTITION_OFFSET + (CHECKPOINT_WORDS * sizeof(uint32_t)); 
 
-    // Loop through the flash storage and restore it to RAM
     for (uint32_t *ram_ptr = (uint32_t *)RAM_START; ram_ptr < (uint32_t *)RAM_END; ram_ptr++) {
-        uint32_t value;
-        if (flash_read(flash_dev, offset, &value, sizeof(uint32_t)) != 0) {
-            printk("Flash read failed at offset 0x%X\n", offset);
-            return -1; 
+        uint32_t data;
+
+        if (flash_read(flash_dev, offset, &data, sizeof(uint32_t)) != 0) {
+            printk("Flash read failed at offset 0x%08X\n", offset);
+            return -1;
         }
-        *ram_ptr = value; // Restore value to RAM
+        printk("Read %d from FLASH 0x%08X to RAM %p\n", data, offset, ram_ptr);
+        *ram_ptr = data;
         offset += sizeof(uint32_t);
     }
     return 0;
 }
 
 void checkpoint_test_flash() {
-    uint32_t before[CHECKPOINT_WORDS] = {0};
-    get_program_state(before);
     checkpoint_create();
+    printk("\n");
     checkpoint_recover();
-    uint32_t after[CHECKPOINT_WORDS] = {0};
-    get_program_state(after);
-    if (memcmp(before, after, CHECKPOINT_WORDS * sizeof(uint32_t)) != 0) {
-        printk("Written data not the same as read data!");
-    }
+    printk("\n");
+    printk("DONE\n");
 }
