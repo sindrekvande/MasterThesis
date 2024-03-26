87d17ffbbd45e52820d2cacc4f272620dafa4b25
sindrekvande <sindrehk2000@hotmail.com>
2024-03-26 16:05:50 +0100

    Part tests update

---
+++ nRF52832/parts/svs/src/main.c
@@ -0,0 +1,23 @@
+#include "svs.h"
+#include "nrfx_gpiote.h"
+#include <zephyr/kernel.h>
+
+#define GPIO_PIN 15
+
+int main() {
+    nrfx_err_t err_code = nrfx_gpiote_init(3);
+    if (err_code != NRFX_SUCCESS){
+        printf("Error (0x%X)\n", err_code); 
+    }
+
+    nrfx_gpiote_output_config_t out_config = NRFX_GPIOTE_DEFAULT_OUTPUT_CONFIG;
+    
+    err_code = nrfx_gpiote_output_configure(GPIO_PIN, &out_config, NULL);
+    if (err_code != NRFX_SUCCESS){
+        printf("Error (0x%X)\n", err_code); 
+    }
+    k_sleep(K_SECONDS(1));
+    nrfx_gpiote_out_set(GPIO_PIN);
+    svs_init();
+    nrfx_gpiote_out_clear(GPIO_PIN);
+}
