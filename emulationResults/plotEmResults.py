ec0558243896537ab15d97fddc3b75b23b7ba1bc
krissch97 <59972870+krissch97@users.noreply.github.com>
2024-05-03 10:31:20 +0200

    test

--- emulationResults/plotEmResults.py
+++ emulationResults/plotEmResults.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.interpolate as sc
+#import scipy.interpolate as sc
 
 
-resultFile = 'emulationResults/Test2.tsv'
+resultFile = 'emulationResults/firstTest.tsv'
 
 VoltageDF = pd.read_csv(resultFile, sep='\t', usecols = ['STORAGE_OUT'],  dtype = np.float32)
 irrDF = pd.read_csv(resultFile, sep='\t', usecols = ['irrValue'],  dtype = np.float32)
 currDF = pd.read_csv(resultFile, sep='\t', usecols = ['CSA_EH_IN'],  dtype = np.float32)
-enDF = pd.read_csv(resultFile, sep='\t', usecols = ['DCDC_EN'],  dtype = np.float32)
+enDF = pd.read_csv(resultFile, sep='\t', usecols = ['MPPT_EN'],  dtype = np.float32)
 
 voltage = []
 irr = []
@@ -20,7 +20,7 @@ for _, v in VoltageDF.itertuples():
     voltage.append(v)
 
 for _, s in irrDF.itertuples():
-    irr.append(s)
+    irr.append(s/333)
 
 for _, i in currDF.itertuples():
     curr.append(i)
@@ -28,9 +28,9 @@ for _, i in currDF.itertuples():
 for _, e in enDF.itertuples():
     en.append(e)
 
-iterator = [[irr[i],curr[i]*voltage[i]] for i in range(64000, 130000, 1)]
+#iterator = [[irr[i],curr[i]*voltage[i]] for i in range(64000, 130000, 1)]
 
-x = np.array([(y/(i/10000 * 0.93 * 4/1000)) for i, y in iterator])
+#x = np.array([(y/(i/10000 * 0.93 * 4/1000)) for i, y in iterator])
 
 #print(np.median(x)*18.2)
 plt.plot(voltage)
