c6aef7318e384abd2841e634e884c3c4ed825f7b
sindrekvande <sindrehk2000@hotmail.com>
2024-05-08 10:36:24 +0200

    Added dcdc consumption plot

--- emulationResults/plotEmResults.py
+++ emulationResults/plotEmResults.py
@@ -6,7 +6,7 @@ import time
 #import scipy.interpolate as sc
 
 
-resultFile = 'emulationResults/Test215half.tsv'
+resultFile = 'emulationResults/Test8.tsv'
 
 VoltageDF = pd.read_csv(resultFile, sep='\t', usecols = ['STORAGE_OUT'],  dtype = np.float32)
 irrDF = pd.read_csv(resultFile, sep='\t', usecols = ['irrValue'],  dtype = np.float32)
@@ -14,6 +14,8 @@ currDF = pd.read_csv(resultFile, sep='\t', usecols = ['CSA_EH_IN'],  dtype = np.
 enDF = pd.read_csv(resultFile, sep='\t', usecols = ['DCDC_OUT_BUF'],  dtype = np.float32)
 en1DF = pd.read_csv(resultFile, sep='\t', usecols = ['EXT_AN_IN_1'],  dtype = np.float32)
 timeDF = pd.read_csv(resultFile, sep='\t', usecols = ['datetime'])
+dcdccsaDF = pd.read_csv(resultFile, sep='\t', usecols = ['CSA_DCDC_OUT'],  dtype = np.float32)
+dcdcbufDF = pd.read_csv(resultFile, sep='\t', usecols = ['DCDC_OUT_BUF'],  dtype = np.float32)
 
 voltage = []
 voltageExp = []
@@ -21,6 +23,9 @@ irr = []
 curr = []
 en = []
 en1 = []
+dcdccsa = []
+dcdcbuf = []
+dcdccurr = []
 timeL = []
 energy = 0
 capacitance = 385 * 10 ** (-3)
@@ -54,17 +59,25 @@ for _, e in enDF.itertuples():
 for _, e in en1DF.itertuples():
     en1.append(e)
 
+for _, c in dcdccsaDF.itertuples():
+    dcdccsa.append(c)
+
+for _, b in dcdcbufDF.itertuples():
+    dcdcbuf.append(b)
+
 
 #iterator = [[irr[i],curr[i]*voltage[i]] for i in range(21000, 40000, 1)]
 
 #x = np.array([(y/(i/10000 * 0.93 * 3/1000)) for i, y in iterator])
 
+dcdccurr = [dcdccsa[i] * dcdcbuf[i] for i in range(len(dcdccsa))]
+
 #print(np.median(x))
 plt.plot(voltage)
 plt.plot(voltageExp)
 plt.plot(irr)
-plt.plot(en)
-#plt.plot(en2)
+plt.plot(dcdccurr)
+#plt.plot(en)
 #plt.plot(x)
 #plt.axhline(np.median(x), color='r')
 #plt.axhline(26, color='orange')
