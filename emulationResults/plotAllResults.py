32c4fb859e0d3b725f819cd813fe6eed34008b29
krissch97 <krissch97@gmail.com>
2024-05-31 18:29:55 +0200

    Update plotAllResults.py

--- emulationResults/plotAllResults.py
+++ emulationResults/plotAllResults.py
@@ -38,12 +38,7 @@ for i in range(0, len(resultFiles), 3):
     for _, v2 in VoltageDF2.itertuples():
         voltage2.append(v2)
 
-    if len(voltage0) > len(voltage1) and len(voltage0) > len(voltage2):
-        timeDF = pd.read_csv(resultFiles[i], sep='\t', usecols = ['datetime'])
-    elif len(voltage1) > len(voltage0) and len(voltage1) > len(voltage2):
-        timeDF = pd.read_csv(resultFiles[i+1], sep='\t', usecols = ['datetime'])
-    else:
-        timeDF = pd.read_csv(resultFiles[i+2], sep='\t', usecols = ['datetime'])
+    timeDF = pd.read_csv(resultFiles[np.argmax(np.array([len(voltage0),len(voltage1),len(voltage2)]))], sep='\t', usecols = ['datetime'])
 
     timeS = np.array([])
     for _, t in timeDF:
@@ -53,12 +48,27 @@ for i in range(0, len(resultFiles), 3):
             timeCurr = datetime.strptime(t+'0000', datetime_str)
         timeS.append(timeCurr)
 
+    voltages = np.array([voltage0, voltage1, voltage2])
+    labels = np.array(['Interval', 'ADC', 'LPCOMP'])
     
-    fig = plt.figure(figsize=(6,2.25))
-
-
-    plt.ylabel('Voltage [V]')
-    plt.xlabel('Time of day')
+    fig = plt.figure(figsize=(6,6))
+    gs = fig.add_gridspec(4, hspace=0)
+    ax = gs.subplots(sharex=True, sharey=False)
+    for i in range(3):
+        ax[i].plot(timeS, voltages[i], label=labels[i])
+        ax[i].set(ylabel='Voltage [V]')
+        ax[i].legend(loc="upper right")
+        ax[i].margins(x=0)
+        ax[i].axhline(3.0, color='grey', ls='--')
+        ax[i].axhline(2.7, color='green', ls='--')
+        if i != 0:
+            ax[i].axhline(1.9, color='orange', ls='--')
+        ax[i].axhline(1.7, color='red', ls='--')
+    ax[3].plot(timeS, voltages, label='LED', color='gold')
+    ax[3].axhline(0.5, color='grey', ls='--')
+    ax[3].set(xlabel='Time of day', ylabel='Brightness [%]')
+    ax[3].legend(loc="upper right")
+    ax[3].margins(x=0)
     plt.legend()
     xformatter = mdates.DateFormatter('%H:%M')
     plt.gcf().axes[0].xaxis.set_major_formatter(xformatter)
