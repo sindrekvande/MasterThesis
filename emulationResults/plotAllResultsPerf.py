9409636d7871005d57b80462394768b6d53f6b44
krissch97 <59972870+krissch97@users.noreply.github.com>
2024-06-10 14:33:04 +0200

    perfPlot

--- emulationResults/plotAllResultsPerf.py
+++ emulationResults/plotAllResultsPerf.py
@@ -21,31 +21,33 @@ metrics = ['Checkpoint', 'Recover', 'Sample', 'Communicate']
 fileMetrics = ['Checkpoint count', 'Recover count', 'Sampling count', 'Communication count']
 labels = ['Interval', 'ADC', 'LPCOMP']
 for i in range(0, len(resultFiles), 3):
-    PerfDF0 = pd.read_csv(resultFiles[i], columns=fileMetrics, sep='\t', dtype=np.int16)
-    PerfDF1 = pd.read_csv(resultFiles[i+1], columns=fileMetrics, sep='\t', dtype=np.int16)
-    PerfDF2 = pd.read_csv(resultFiles[i+2], columns=fileMetrics, sep='\t', dtype=np.int16)
+    PerfDF0 = pd.read_csv(resultFiles[i], sep='\t')
+    PerfDF1 = pd.read_csv(resultFiles[i+1], sep='\t')
+    PerfDF2 = pd.read_csv(resultFiles[i+2], sep='\t')
     
 
-    barResults = []
-    barResults[0] = [(PerfDF0.at[-1, 1] - PerfDF0.at[0, 1]), (PerfDF0.at[-1, 2] - PerfDF0.at[0, 2]), (PerfDF0.at[-1, 3] - PerfDF0.at[0, 3]), (PerfDF0.at[-1, 4] - PerfDF0.at[0, 4])]
-    barResults[1] = [(PerfDF1.at[-1, 1] - PerfDF1.at[0, 1]), (PerfDF1.at[-1, 2] - PerfDF1.at[0, 2]), (PerfDF1.at[-1, 3] - PerfDF1.at[0, 3]), (PerfDF1.at[-1, 4] - PerfDF1.at[0, 4])]
-    barResults[2] = [(PerfDF2.at[-1, 1] - PerfDF2.at[0, 1]), (PerfDF2.at[-1, 2] - PerfDF2.at[0, 2]), (PerfDF2.at[-1, 3] - PerfDF2.at[0, 3]), (PerfDF2.at[-1, 4] - PerfDF2.at[0, 4])]
+    barResults = [[], [], []]
+    barResults[0] = [(PerfDF0.iat[-1, 1] - PerfDF0.iat[0, 1]), (PerfDF0.iat[-1, 2] - PerfDF0.iat[0, 2]), (PerfDF0.iat[-1, 3] - PerfDF0.iat[0, 3]), (PerfDF0.iat[-1, 4] - PerfDF0.iat[0, 4])]
+    barResults[1] = [(PerfDF1.iat[-1, 1] - PerfDF1.iat[0, 1]), (PerfDF1.iat[-1, 2] - PerfDF1.iat[0, 2]), (PerfDF1.iat[-1, 3] - PerfDF1.iat[0, 3]), (PerfDF1.iat[-1, 4] - PerfDF1.iat[0, 4])]
+    barResults[2] = [(PerfDF2.iat[-1, 1] - PerfDF2.iat[0, 1]), (PerfDF2.iat[-1, 2] - PerfDF2.iat[0, 2]), (PerfDF2.iat[-1, 3] - PerfDF2.iat[0, 3]), (PerfDF2.iat[-1, 4] - PerfDF2.iat[0, 4])]
 
+    #print(barResults)
     barWidth = 0.3
-    x = np.arange(len(metrics))
+    mlen = np.arange(len(metrics))
     fig = plt.figure(figsize=(4.67,4))
     colors = ['#80c2c2', '#008585', '#74a892']#['#0089B3', '#00556F', '#00C4FF']
 
-    for i in range(3):
-        plt.bar(x+barWidth*(i-1), barResults[i], width=barWidth, label=labels[i], color=colors[i])
-        for j, v in enumerate(barResults[i]):
-            plt.text(j+barWidth*(i-1), v, str(v), color=colors[i], horizontalalignment='center', verticalalignment='bottom')
-    plt.xticks(x, metrics)
+    for x in range(3):
+        plt.bar(mlen+barWidth*(x-1), barResults[x], width=barWidth, label=labels[x], color=colors[x])
+        for j, v in enumerate(barResults[x]):
+            plt.text(j+barWidth*(x-1), v, str(v), color=colors[x], horizontalalignment='center', verticalalignment='bottom')
+    plt.xticks(mlen, metrics)
     plt.ylabel('Number of times')
     plt.xlabel('Metric')
+    plt.xlim(-1,4)
     plt.legend(loc='best')
     fig.tight_layout()
-    plt.savefig('emulationResults/allPerfPlots/'+resultFiles[i][-19:-4]+'.pdf', format='pdf', bbox_inches="tight")
+    plt.savefig('emulationResults/allPerfPlots/barPlot'+resultFiles[i][-19:-4]+'.pdf', format='pdf', bbox_inches="tight")
     #plt.show()
     plt.cla()
     plt.close()
